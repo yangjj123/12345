d094a28ca66a958e2ff439df3a55f708d3b55415
yj <you@example.com>
2016-03-17 21:09:30 +0800

    the third

--- fuli.java
+++ fuli.java
@@ -9,8 +9,9 @@ public class fuli {
 		// TODO Auto-generated method stub
 		Scanner caculate = new Scanner(System.in);
 		int a = 0;
-		while (a == 0 || a == 1 || a == 2 || a == 3) {
-			System.out.println("请选择1/2/3：\n1.复利计算终值2.复利计算本金3.单利计算本息");
+		while (a == 0) {
+			System.out
+					.println("请选择1/2/3/4：\n1.复利计算终值\n2.复利计算本金\n3.单利计算本息\n4.计算获取复利终值的年限\n5.计算报酬率\n6.每年或每月定期投资");
 			int a1 = caculate.nextInt();
 			if (a1 == 1) {
 				fuli.CFutureValue();
@@ -23,14 +24,27 @@ public class fuli {
 			else if (a1 == 3) {
 				fuli.PrincipalAndInterest();
 				break;
+			} else if (a1 == 4) {
+				fuli.CYear();
+				break;
+			} else if (a1 == 5) {
+				fuli.Rate();
+				break;
+			} else if (a1 == 6) {
+				fuli.regular();
+				break;
 			} else {
-				System.out.println("\n您的输入错误");
+
+				System.out.println("\n您的输入错误,请重新选择：");
 				a1 = a;
 			}
 		}
+		System.out
+				.println("\n感谢您的使用，希望下次再来，如果有什么建议可以向我提出，谢谢！\n          --by 杨晶晶");
+
 	}
 
-	// 复利计算终值
+	// 1.复利计算终值
 	static void CFutureValue() {
 		Scanner fuli = new Scanner(System.in);
 		System.out.println("请输入本金：");
@@ -39,19 +53,18 @@ public class fuli {
 		int year = fuli.nextInt(); // 存钱年数
 		System.out.println("请输入利率：");
 		double r = fuli.nextDouble(); // 利率
-		double sum = 1;
+		double FutureValue = 1;
 		for (int i = 1; i <= year; i++) {
-			sum = (1 + r) * sum;
+			FutureValue = (1 + r) * FutureValue;
 		}
-		System.out.println("总额为：" + sum * PresentValue);
+		System.out.println("总额为：" + FutureValue * PresentValue);
 	}
 
-	// 复利计算本金
+	// 2.复利计算本金
 	static void CPresentValue() {
-
 		Scanner fuli = new Scanner(System.in);
 		System.out.println("请输入终值：");
-		double sum = fuli.nextDouble(); // 终值
+		double FutureValue = fuli.nextDouble(); // 复利终值
 		System.out.println("请输入存款年数：");
 		int year = fuli.nextInt(); // 存钱年数
 		System.out.println("请输入利率：");
@@ -60,21 +73,108 @@ public class fuli {
 		for (int i = 1; i <= year; i++) {
 			PresentValue = (1 + r) * PresentValue;
 		}
-		System.out.println("总额为：" + sum * (1 / PresentValue));
+		System.out.println("总额为：" + FutureValue * (1 / PresentValue));
 	}
 
-	// 单利计算本息
+	// 3.单利计算本息
 	static void PrincipalAndInterest() {
 		Scanner danli = new Scanner(System.in);
 		System.out.println("请输入本金：");
 		double PresentValue = danli.nextDouble(); // 本金
 		System.out.println("请输入存款年数：");
-		int Annuity = danli.nextInt(); // 存钱年数
+		int year = danli.nextInt(); // 存钱年数
 		System.out.println("请输入利率：");
 		double r = danli.nextDouble(); // 利率
-		double sum1 = 1;
-		sum1 = (1 + r * Annuity) * PresentValue;
-		System.out.println("本息为：" + sum1);
+		double FutureValue = 1;
+		FutureValue = (1 + r * year) * PresentValue;
+		System.out.println("本息为：" + FutureValue);
+	}
+
+	// 4.计算年限
+	static void CYear() {
+		Scanner fuli = new Scanner(System.in);
+		System.out.println("请输入本金：");
+		double PresentValue = fuli.nextDouble(); // 本金
+		System.out.println("请输入复利终值：");
+		double FutureValue = fuli.nextInt(); // 复利终值
+		System.out.println("请输入利率：");
+		double r = fuli.nextDouble(); // 报酬率
+		int year = (int) ((Math.log(FutureValue) / Math.log(1 + r)) - (Math
+				.log(PresentValue) / Math.log(1 + r)));
+		System.out.println("所需年限为：" + year);
+
+	}
+
+	// 5.计算报酬率
+	static void Rate() {
+		Scanner fuli = new Scanner(System.in);
+		int a = 0;
+		while (a == 0) {
+			System.out.println("是否选择计算翻倍：\n1、翻倍\n2、不翻倍");
+			int a1 = fuli.nextInt();
+			if (a1 == 2) {
+				System.out.println("请输入本金：");
+				double PresentValue = fuli.nextDouble(); // 本金
+				System.out.println("请输入复利终值：");
+				double FutureValue = fuli.nextInt(); // 复利终值
+				System.out.println("请输入存款年数：");
+				int year = fuli.nextInt(); // 存钱年数
+				double r = (Math.pow(FutureValue / PresentValue, 1.0 / year)) - 1;
+				System.out.println("总额为：" + r);
+				break;
+			} else if (a1 == 1) {
+				System.out.println("请输入几年后：");
+				int year = fuli.nextInt();
+				System.out.println("翻几番：");
+				int FutureValue = fuli.nextInt();
+				double rate = 72.0 / (year / FutureValue); // 72法则
+				System.out.println(year + "年后本金翻倍的年利率至少为：" + rate);
+				break;
+			} else {
+				System.out.println("\n您的输入错误，请重新选择");
+				a1 = a;
+			}
+		}
+
+	}
+
+	// 6.计算按每年或每月定期投资的终值
+	static void regular() {
+		Scanner fuli = new Scanner(System.in);
+		int a = 0;
+		while (a == 0) {
+			System.out.println("请选择\n1.按年投资\n2.按月投资");
+			int a1 = fuli.nextInt();
+			if (a1 == 1) {
+				System.out.println("要投资的本金：");
+				double P = fuli.nextDouble();
+				System.out.println("获得的年回报率：");
+				double r = fuli.nextDouble();
+				System.out.println("投资年限(年)：");
+				double Y = fuli.nextInt();
+
+				double F = P * (Math.pow(1 + r, Y) - 1) / r;
+				System.out.println("按年投资的年金终值为：" + F);
+
+				break;
+			}
+
+			else if (a1 == 2) {
+				System.out.println("要投资的本金：");
+				double P = fuli.nextDouble();
+				System.out.println("获得的月回报率：");
+				double r = fuli.nextDouble();
+				System.out.println("投资年限(月)：");
+				double Y = fuli.nextInt();
+				double F = P * (Math.pow(1 + r / 12.0, Y * 12) - 1) / (r / 12);
+				System.out.println("按月投资的年金终值为：" + F);
+				break;
+			} else {
+				System.out.println("\n您的输入错误，请重新选择");
+				a1 = a;
+			}
+		}
+
 	}
 
 }
